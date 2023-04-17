3822182df7ab49f936d865e386c802d0a5464641
Zunnun <zunnunjuraev211@gmail.com>
2023-04-17 17:50:19 +0500

    init new project

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -4,4 +4,8 @@ using TodoList.Models;
 Class1 myClass = new Class1();
 myClass.Age = 21;
 myClass.Name = "Toto";
-System.Console.WriteLine(myClass.Age + " " + myClass.Name);
+Console.WriteLine(myClass.Age + " " + myClass.Name);
+System.Console.WriteLine("Hello world");
+for(int i = 0;i<100;i++){
+    System.Console.WriteLine(i);
+}
